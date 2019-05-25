4403fdb13671d4e7225736c08641ee264744fb11
Federico Bustos Fierro <federico0812@gmail.com>
2019-05-25 17:48:00 -0300

    new exercises

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int lados1, lado2;
+
+            Console.WriteLine("Ingresar la base del triangulo");
+
+            lados1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar el valor de los lados del triangulo");
+
+            lado2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La superficie del triangulo es:   {lados1 * lado2 / 2  }");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ ejercicio6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2, num3, num4, num5, num6;
+
+            Console.WriteLine("Ingresa los 6 numeros");
+
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num3 = int.Parse(Console.ReadLine());
+            num4 = int.Parse(Console.ReadLine());
+            num5 = int.Parse(Console.ReadLine());
+           num6 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El promedio es: {num1 + num2 + num3 + num4 + num5 + num6 / 6}");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
