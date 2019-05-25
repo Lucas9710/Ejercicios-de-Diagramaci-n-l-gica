4878ca18e39c2fab0592a07bb35d36d731d0725e
Federico Bustos Fierro <federico0812@gmail.com>
2019-05-25 17:42:44 -0300

    ejercicio8

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int total, parcial;
+
+            Console.WriteLine("Ingresar el numero total");
+
+            total = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar el numero parcial");
+            parcial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"el porcentaje que representa el numero parcial al numero total es: {parcial * 100 / total }");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ ejercicio8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercicio8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int fecha, anio, mes, dia;
+
+            Console.WriteLine("Ingrese la fecha");
+            fecha = int.Parse(Console.ReadLine());
+
+            dia = fecha / 1000000;
+            mes = (fecha % 1000000) / 10000;
+            anio = fecha % 10000;
+
+            Console.WriteLine("la fecha es: " + dia + " " + " " + mes + " " + anio);
+
+            Console.ReadKey(true);
+
+
+            
+
+
+
+
+        }
+    }
+}
